7d305b34915d95784bef0eeec91a33980dcf39f2
App Academy Student <student@appacademy.io>
2015-12-03 10:27:54 -0800

    prototype methods (up to append)

--- jquerylite/js/jquery_lite.js
+++ jquerylite/js/jquery_lite.js
@@ -6,9 +6,43 @@
     for(var i = 0; i < nodelist.length; i++){
       nodeArray[i] = nodelist[i];
     }
-    console.log(nodeArray);
+    // console.log(nodeArray);
+    return new DOMNodeCollection(nodeArray);
   }
   window.onload = function(){
-    $l("li");
+    $l("li").html("I am innerHTML");
+    $l("li").append("COOOOOOL");
+    var kyle = $l("div");
+    $l("li").append(kyle);
+    // $l("li").empty();
   };
+
+  function DOMNodeCollection (HTMLElements) {
+    this.HTMLElements = HTMLElements;
+  }
+
+  DOMNodeCollection.prototype.html = function (string) {
+    if (string === "undefined") {
+      return this.HTMLElements[0].innerHTML();
+    } else {
+      this.HTMLElements.forEach(function(el){
+        el.innerHTML = string;
+      });
+    }
+  };
+
+  DOMNodeCollection.prototype.empty = function (){
+    this.html("");
+  };
+
+  DOMNodeCollection.prototype.append = function (contents){
+    if(typeof contents === "string"){
+      this.html(contents);
+    } else if(contents instanceof DOMNodeCollection) {
+      console.log("DOM NODE COLLECTION");
+    } else {
+      console.log("NETIHER");
+    }
+  };
+
 }());
